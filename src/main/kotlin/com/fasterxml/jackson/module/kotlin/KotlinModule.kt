d006d7cbd917e65c3f3e2246cd5ece4d0d839c95
Gunnar Schulze <gunnar.schulze@invenium.io>
2019-05-28 16:15:40 +0200

    make compatible with Kotlin 1.3

--- src/main/kotlin/com/fasterxml/jackson/module/kotlin/KotlinModule.kt
+++ src/main/kotlin/com/fasterxml/jackson/module/kotlin/KotlinModule.kt
@@ -1,135 +1,138 @@
 package com.fasterxml.jackson.module.kotlin
 
 import com.fasterxml.jackson.annotation.JsonCreator
-import com.fasterxml.jackson.databind.Module.SetupContext
 import com.fasterxml.jackson.databind.introspect.*
 import com.fasterxml.jackson.databind.module.SimpleModule
 import java.lang.reflect.Constructor
 import java.lang.reflect.Method
 import java.lang.reflect.Modifier
-import java.util.HashSet
-import kotlin.jvm.internal.KotlinClass
-import kotlin.reflect.*
-import kotlin.reflect.jvm.javaMethod
+import java.util.*
+import kotlin.reflect.KParameter
+import kotlin.reflect.full.companionObject
+import kotlin.reflect.full.declaredFunctions
+import kotlin.reflect.full.primaryConstructor
+import kotlin.reflect.jvm.internal.KotlinReflectionInternalError
 import kotlin.reflect.jvm.kotlinFunction
 
-public class KotlinModule() : SimpleModule(PackageVersion.VERSION) {
-    companion object {
-        private val serialVersionUID = 1L;
-    }
+private const val metadataFqName = "kotlin.Metadata"
 
-    val requireJsonCreatorAnnotation: Boolean = false
+val Class<*>.isKotlinClass: Boolean
+	get() {
+		return this.declaredConstructors.any {
+			this.declaredAnnotations.singleOrNull { it.annotationClass.java.name == metadataFqName } != null
+		}
+	}
 
-    val impliedClasses = HashSet<Class<*>>(setOf(
-            Pair::class.java,
-            Triple::class.java
-    ))
+class KotlinModule : SimpleModule(PackageVersion.VERSION) {
 
-    override public fun setupModule(context: SetupContext) {
-        super.setupModule(context)
+	companion object {
+		private const val serialVersionUID = 1L
+	}
 
-        fun addMixin(clazz: Class<*>, mixin: Class<*>) {
-            impliedClasses.add(clazz)
-            context.setMixInAnnotations(clazz, mixin)
-        }
+	val requireJsonCreatorAnnotation: Boolean = false
 
-        context.appendAnnotationIntrospector(KotlinNamesAnnotationIntrospector(this))
+	private val impliedClasses = HashSet<Class<*>>(setOf(
+			Pair::class.java,
+			Triple::class.java
+	))
 
-        // ranges
-        addMixin(IntRange::class.java, ClosedRangeMixin::class.java)
-        addMixin(CharRange::class.java, ClosedRangeMixin::class.java)
-        addMixin(LongRange::class.java, ClosedRangeMixin::class.java)
-    }
+	override fun setupModule(context: SetupContext) {
+		super.setupModule(context)
+
+		fun addMixin(clazz: Class<*>, mixin: Class<*>) {
+			impliedClasses.add(clazz)
+			context.setMixInAnnotations(clazz, mixin)
+		}
+
+		context.appendAnnotationIntrospector(KotlinNamesAnnotationIntrospector(this))
+
+		// ranges
+		addMixin(IntRange::class.java, ClosedRangeMixin::class.java)
+		addMixin(CharRange::class.java, ClosedRangeMixin::class.java)
+		addMixin(LongRange::class.java, ClosedRangeMixin::class.java)
+	}
 }
 
 
-internal class KotlinNamesAnnotationIntrospector(val module: KotlinModule) : NopAnnotationIntrospector() {
-    /*
-    override public fun findNameForDeserialization(annotated: Annotated?): PropertyName? {
-        // This should not do introspection here, only for explicit naming by annotations
-        return null
-    }
-    */
+internal open class KotlinNamesAnnotationIntrospector(val module: KotlinModule) : NopAnnotationIntrospector() {
 
-    // since 2.4
-    override public fun findImplicitPropertyName(member: AnnotatedMember): String? {
-        if (member is AnnotatedParameter) {
-            return findKotlinParameterName(member)
-        }
-        return null
-    }
+	// since 2.4
+	override fun findImplicitPropertyName(member: AnnotatedMember): String? {
+		if (member is AnnotatedParameter) {
+			return findKotlinParameterName(member)
+		}
+		return null
+	}
 
-    @Suppress("UNCHECKED_CAST")
-    override public fun hasCreatorAnnotation(member: Annotated): Boolean {
-        // don't add a JsonCreator to any constructor if one is declared already
+	@Suppress("UNCHECKED_CAST")
+	override fun hasCreatorAnnotation(member: Annotated): Boolean {
+		// don't add a JsonCreator to any constructor if one is declared already
 
-        if (member is AnnotatedConstructor) {
-            // if has parameters, is a Kotlin class, and the parameters all have parameter annotations, then pretend we have a JsonCreator
-            if (member.getParameterCount() > 0 && member.getDeclaringClass().getAnnotation(KotlinClass::class.java) != null) {
-                val kClass = (member.getDeclaringClass() as Class<Any>).kotlin
-                val kConstructor = (member.getAnnotated() as Constructor<Any>).kotlinFunction
+		if (member is AnnotatedConstructor) {
+			// if has parameters, is a Kotlin class, and the parameters all have parameter annotations, then pretend we have a JsonCreator
+			if (member.parameterCount > 0 && member.declaringClass.isKotlinClass) {
+				val kClass = (member.declaringClass as Class<Any>).kotlin
+				val kConstructor = (member.annotated as Constructor<Any>).kotlinFunction
 
-                if (kConstructor != null) {
-                    val isPrimaryConstructor = kClass.primaryConstructor == kConstructor ||
-                                (kClass.primaryConstructor == null && kClass.constructors.size == 1)
-                    val anyConstructorHasJsonCreator = kClass.constructors.any { it.annotations.any { it.annotationClass.java == JsonCreator::class.java } } // member.getDeclaringClass().getConstructors().any { it.getAnnotation() != null }
+				if (kConstructor != null) {
+					val isPrimaryConstructor = kClass.primaryConstructor == kConstructor ||
+							(kClass.primaryConstructor == null && kClass.constructors.size == 1)
+					val anyConstructorHasJsonCreator = kClass.constructors.any { constructor ->
+						constructor.annotations.any { it.annotationClass.java == JsonCreator::class.java }
+					}
 
-                    val anyCompanionMethodIsJsonCreator = member.getContextClass().rawType.kotlin.companionObject?.declaredFunctions?.any {
-                        it.annotations.any { it.annotationClass.java == JvmStatic::class.java } &&
-                                it.annotations.any { it.annotationClass.java == JsonCreator::class.java }
-                    } ?: false
-                    val anyStaticMethodIsJsonCreator = member.getContextClass().rawType.declaredMethods.any {
-                        val isStatic = Modifier.isStatic(it.modifiers)
-                        val isCreator = it.declaredAnnotations.any { it.annotationClass.java == JsonCreator::class.java }
-                        isStatic && isCreator
-                    }
+					val anyCompanionMethodIsJsonCreator = member.contextClass.rawType.kotlin.companionObject?.declaredFunctions?.any { function ->
+						function.annotations.any { it.annotationClass.java == JvmStatic::class.java } &&
+								function.annotations.any { it.annotationClass.java == JsonCreator::class.java }
+					} ?: false
+					val anyStaticMethodIsJsonCreator = member.contextClass.rawType.declaredMethods.any { method ->
+						val isStatic = Modifier.isStatic(method.modifiers)
+						val isCreator = method.declaredAnnotations.any { it.annotationClass.java == JsonCreator::class.java }
+						isStatic && isCreator
+					}
 
-                    val areAllParametersValid = kConstructor.parameters.size == kConstructor.parameters.count { it.name != null }
-                    val implyCreatorAnnotation = isPrimaryConstructor && !(anyConstructorHasJsonCreator || anyCompanionMethodIsJsonCreator || anyStaticMethodIsJsonCreator) && areAllParametersValid
+					val areAllParametersValid = kConstructor.parameters.size == kConstructor.parameters.count { it.name != null }
 
-                    return implyCreatorAnnotation
-                }
-            }
-        }
-        return false
-    }
+					return isPrimaryConstructor && !(anyConstructorHasJsonCreator || anyCompanionMethodIsJsonCreator || anyStaticMethodIsJsonCreator) && areAllParametersValid
+				}
+			}
+		}
+		return false
+	}
 
-    @Suppress("UNCHECKED_CAST")
-    protected fun findKotlinParameterName(param: AnnotatedParameter): String? {
-        if (param.getDeclaringClass().getAnnotation(KotlinClass::class.java) != null) {
-            val member = param.getOwner().getMember()
-            val name = if (member is Constructor<*>) {
-                val ctor = (member as Constructor<Any>)
-                val ctorParmCount = ctor.parameterTypes.size
-                val ktorParmCount = ctor.kotlinFunction?.parameters?.size ?: 0
-                if (ktorParmCount > 0 && ktorParmCount == ctorParmCount) {
-                    ctor.kotlinFunction?.parameters?.get(param.index)?.name
-                } else {
-                    null
-                }
-            } else if (member is Method) {
-                try {
-                    val temp = member.kotlinFunction
+	@Suppress("UNCHECKED_CAST")
+	protected fun findKotlinParameterName(param: AnnotatedParameter): String? {
+		if (param.declaringClass.isKotlinClass) {
+			val member = param.owner.member
+			return if (member is Constructor<*>) {
+				val ctor = (member as Constructor<Any>)
+				val ctorParamCount = ctor.parameterTypes.size
+				val ktorParamCount = ctor.kotlinFunction?.parameters?.size ?: 0
+				if (ktorParamCount > 0 && ktorParamCount == ctorParamCount) {
+					ctor.kotlinFunction?.parameters?.get(param.index)?.name
+				} else {
+					null
+				}
+			} else if (member is Method) {
+				try {
+					val temp = member.kotlinFunction
 
-                    val firstParamKind = temp?.parameters?.firstOrNull()?.kind
-                    val idx = if (firstParamKind != KParameter.Kind.VALUE) param.index + 1 else param.index
-                    val parmCount = temp?.parameters?.size ?: 0
-                    if (parmCount > idx) {
-                        temp?.parameters?.get(idx)?.name
-                    }
-                    else {
-                        null
-                    }
-                }
-                catch (ex: KotlinReflectionInternalError) {
-                    null
-                }
-            } else {
-                null
-            }
-            return name
-        }
-        return null
-    }
+					val firstParamKind = temp?.parameters?.firstOrNull()?.kind
+					val idx = if (firstParamKind != KParameter.Kind.VALUE) param.index + 1 else param.index
+					val paramCount = temp?.parameters?.size ?: 0
+					if (paramCount > idx) {
+						temp?.parameters?.get(idx)?.name
+					} else {
+						null
+					}
+				} catch (ex: KotlinReflectionInternalError) {
+					null
+				}
+			} else {
+				null
+			}
+		}
+		return null
+	}
 
 }
